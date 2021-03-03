9172662b1112300775b38cc2a97c07f85910be37
Lucas Lima <lucaslima@gmail.com>
2021-03-03 12:31:17 -0300

    Primeiro Commit

---
+++ Api_CQRS/Recursos/Commands/ProdutoDeleteCommand.cs
@@ -0,0 +1,15 @@
+﻿using Api_CQRS.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api_CQRS.Recursos.Commands
+{
+    public class ProdutoDeleteCommand : IRequest<Produto>
+    {
+        public int Id { get; set; }
+
+    }
+}
